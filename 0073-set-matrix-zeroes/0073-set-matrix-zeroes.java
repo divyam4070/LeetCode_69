4d645a21ccfc1e4987de74c8de6e0f5f59afeb4a
Divyam <71552681+divyam4070@users.noreply.github.com>
2025-03-18 12:15:02 +0530

    Time: 7 ms (5.25%), Space: 45.9 MB (29.01%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,29 +1,59 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        HashSet<Integer> rows = new HashSet<>();
-        HashSet<Integer> cols = new HashSet<>();
-        int m = matrix.length;
-        int n = matrix[0].length;
+        boolean firstrowhaszero = false;
+        boolean firstcolhaszero = false;
+        
 
-
-        for(int i = 0; i<m; i++){
-            for(int j = 0;j <n; j++){
-                if(matrix[i][j] == 0){
-                    rows.add(i);
-                    cols.add(j);
+        for(int i = 0; i<matrix.length; i++){
+            if(matrix[i][0] == 0){
+                firstcolhaszero = true;
+                break;
+            }
+        }
+    
+        for(int i = 0; i<matrix[0].length; i++){
+            if(matrix[0][i] == 0){
+                firstrowhaszero = true;
+                break;
+            }
+        }
+      
+        for(int i = 1;i <matrix.length; i++){
+            for(int j = 1; j<matrix[0].length; j++){
+                if(matrix[i][j]==0){
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
                 }
             }
         }
-        for(Integer r : rows){ 
-            for(int i = 0; i<n;i++){
-                matrix[r][i] =0;
-            } 
+        // System.out.print(Arrays.deepToString(matrix));
+        for(int i = 1; i<matrix.length; i++){
+            if(matrix[i][0] == 0){
+                for(int j = 0; j< matrix[0].length; j++){
+                    matrix[i][j] = 0;
+                }
+            }
         }
-        for(Integer c : cols){
-            for(int i = 0; i< m; i++){
-                matrix[i][c] = 0;
+
+        for(int i = 1; i<matrix[0].length; i++){
+            if(matrix[0][i] == 0){
+                for(int j = 0; j<matrix.length; j++){
+                    matrix[j][i]=0;
+                }
+            }
+        }
+        System.out.print(Arrays.deepToString(matrix));
+        if(firstrowhaszero){
+            for(int i = 0; i<matrix[0].length; i++){
+                matrix[0][i] = 0;
+            }
+        }
+        if(firstcolhaszero){
+            for(int i = 0; i<matrix.length; i++){
+                matrix[i][0] = 0;
             }
         }
         return;
+        
     }
 }
