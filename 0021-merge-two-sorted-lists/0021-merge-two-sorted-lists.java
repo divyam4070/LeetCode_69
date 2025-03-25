e5dec748f5d18a8d35fead60e2d8e801e58d5c98
Divyam <71552681+divyam4070@users.noreply.github.com>
2025-03-25 22:18:41 +0530

    Time: 0 ms (100%), Space: 43.3 MB (8.6%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -15,23 +15,23 @@ class Solution {
         while(list1!=null || list2!=null)
         {
             
-           if(list1==null){
-            t.next=list2;
-            t=t.next;
-            list2=list2.next;
-            break;
-           }
-           if(list2 == null)
+           if(list1==null)
            {
-            t.next = list1;
-            t = t.next;
-            list1 = list1.next;
-           break;
+                t.next=list2;
+                t=t.next;
+                list2=list2.next;
+                break;
+           }
+
+           else if(list2 == null)
+           {
+                t.next = list1;
+                t = t.next;
+                list1 = list1.next;
+                break;
            }
                       
-           
-           
-            if(list1.val<list2.val)
+            else if(list1.val<list2.val)
              {
                 t.next=list1;
                 t=t.next;
@@ -44,10 +44,6 @@ class Solution {
              }
                                                                         
         }
-
-
-        
-        
         return dummy.next;
     }
 }   
