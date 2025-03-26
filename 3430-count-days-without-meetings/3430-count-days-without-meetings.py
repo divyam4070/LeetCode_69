555002a4831a9d5a71ddf959fd6c96eff648cc05
Divyam <71552681+divyam4070@users.noreply.github.com>
2025-03-26 09:21:31 +0530

    Time: 83 ms (95.12%), Space: 52.9 MB (27.88%) - LeetHub

--- 3430-count-days-without-meetings/3430-count-days-without-meetings.py
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.py
@@ -1,11 +1,11 @@
 class Solution:
     def countDays(self, days: int, meetings: List[List[int]]) -> int:
-        meetings.sort(key = lambda x: x[0])
+        
         if days == 1:
             return 0
         res = 0
         bada = 0
-        for i in meetings:
+        for i in sorted(meetings, key=lambda x: x[0]):
             if i[0]>bada+1:
                 res+=i[0]-bada-1
             bada = max(bada, i[1])
