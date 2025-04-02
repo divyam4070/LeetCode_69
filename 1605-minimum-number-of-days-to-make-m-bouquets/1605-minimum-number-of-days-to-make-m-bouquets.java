8990a0a767a65a58c28fdf0f859ad82149ed998d
Divyam <71552681+divyam4070@users.noreply.github.com>
2025-04-02 11:21:46 +0530

    Time: 25 ms (10.84%), Space: 60 MB (5.54%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.java
@@ -1,34 +1,25 @@
 class Solution {
-    public int minDays(int[] bloomDay, int m, int k) {
-        if((long)m*k>bloomDay.length){
-            return -1;
-        }
-        int l = Integer.MAX_VALUE;
-        int r = Integer.MIN_VALUE;
-        for(int i:bloomDay){
-            if(l>i){
-                l=i;
-            }
-            if(r<i){
-                r=i;
-            }
-        }
-       
-        while(l<r){
-            int mid = l+(r-l)/2;
-    
-           
-            if(getBouquetCount(bloomDay, k, mid)>=m){
-                r = mid;
-            }
+  public int minDays(int[] bloomDay, int m, int k) {
+    if (bloomDay.length < (long) m * k)
+      return -1;
 
-            else{
-                l = mid+1;
-            }
-        }
-        return l;
+    int l = Arrays.stream(bloomDay).min().getAsInt();
+    int r = Arrays.stream(bloomDay).max().getAsInt();
+
+    while (l < r) {
+      final int mid = (l + r) / 2;
+      if (getBouquetCount(bloomDay, k, mid) >= m)
+        r = mid;
+      else
+        l = mid + 1;
     }
-      private int getBouquetCount(int[] bloomDay, int k, int waitingDays) {
+
+    return l;
+  }
+
+  // Returns the number of bouquets (k flowers needed) can be made after the
+  // `waitingDays`..
+  private int getBouquetCount(int[] bloomDay, int k, int waitingDays) {
     int bouquetCount = 0;
     int requiredFlowers = k;
     for (final int day : bloomDay)
