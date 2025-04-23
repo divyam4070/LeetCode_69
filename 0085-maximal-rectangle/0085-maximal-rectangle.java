d6ef1ab5c4d600027c485f20721328a54575fc26
Divyam <71552681+divyam4070@users.noreply.github.com>
2025-04-23 10:43:16 +0530

    Time: 14 ms (80.31%), Space: 46.8 MB (42.99%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int maximalRectangle(char[][] matrix) {
+        int rows = matrix.length;
+        int res = 0;
+        int cols = matrix[0].length;
+        int[] hehe = new int[cols];
+        for(int i = 0; i<rows; i++){
+            for(int j=0;j<cols;j++){
+                if(matrix[i][j]=='1'){
+                    hehe[j]+=1;
+                }
+                else{
+                    hehe[j] = 0;
+                }
+            }
+            int bachao = helper(hehe);
+            res = Math.max(res, bachao);
+        }
+        return res;
+    }
+    static int helper(int[] heights){
+        int len = heights.length;
+        Stack<Integer> s = new Stack<>();
+        int maxArea = 0;
+        for(int i = 0 ; i<=len; i++){
+            int h = (i==len? 0: heights[i]);
+            if(s.isEmpty()|| h>=heights[s.peek()]){
+                s.push(i);
+            } else{
+                int tp = s.pop();
+                maxArea = Math.max(maxArea, heights[tp]*(s.isEmpty()? i: i-1-s.peek()));
+                i--;
+            }
+        }
+        return maxArea;
+    }
+}
