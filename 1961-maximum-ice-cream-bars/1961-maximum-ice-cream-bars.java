2815914cbba8b5d91fec890466e6cf1eb530e000
Divyam <71552681+divyam4070@users.noreply.github.com>
2025-05-19 10:28:10 +0530

    Time: 33 ms (63.13%), Space: 61.8 MB (13.32%) - LeetHub

--- 1961-maximum-ice-cream-bars/1961-maximum-ice-cream-bars.java
+++ 1961-maximum-ice-cream-bars/1961-maximum-ice-cream-bars.java
@@ -1,6 +1,6 @@
 class Solution {
     public int maxIceCream(int[] costs, int coins) {
-        int sum = 0;
+        // int sum = 0;
         int ans = 0;
         Arrays.sort(costs);
         for(int i = 0 ; i<costs.length; i++){
