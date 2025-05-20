4021df8e1358e21c7d81d7aef1c6712d5ba30c52
Divyam <71552681+divyam4070@users.noreply.github.com>
2025-05-20 12:25:17 +0530

    Time: 7 ms (67.27%), Space: 64.6 MB (50.52%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.java
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.java
@@ -11,31 +11,29 @@ class Solution {
                 idx = i;
             }
         }
-        System.out.println(max);
+
         for(int i=0; i<arrays.size(); i++){
             if(arrays.get(i).get(0)<min && i!=idx){
                 min = arrays.get(i).get(0);
             }
         }
-        System.out.println(min);
-        System.out.print(idx);
+
+        int ans1 = max-min;
         if(arrays.get(idx).get(0)>=min){
             return max-min;
         }
         else{
             min = arrays.get(idx).get(0);
             max = Integer.MIN_VALUE;
-            System.out.println(max);
+      
             for(int i=0; i<arrays.size(); i++){
-                System.out.print("lol");
+     
                 if(arrays.get(i).get(arrays.get(i).size()-1)>max && i!=idx){
                     max = arrays.get(i).get(arrays.get(i).size()-1);
                 }
             
-            }
-            System.out.println(max);
-            System.out.println(min);
-            return max-min;
+            }            
+            return Math.max(ans1,(max-min));
         }
             
         
