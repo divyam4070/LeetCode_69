0e9b8524c6693df256cd8d97170366625b47f846
Divyam <71552681+divyam4070@users.noreply.github.com>
2025-05-21 11:03:11 +0530

    Time: 9 ms (99.34%), Space: 48.5 MB (74.01%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -4,17 +4,15 @@ class Solution {
         Arrays.sort(s);
         int gi = 0;
         int si = 0;
-        int ans = 0;
+        
         while(gi<g.length && si<s.length){
             if(g[gi]<=s[si]){
-                si++;
+               
                 gi++;
-                ans++;
+                
             }
-            else{
-                si++;
-            }
+            si++;
         }
-        return ans;
+        return gi;
     }
 }
