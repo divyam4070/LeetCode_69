2f9744362853bd9813b54c44e6280f1db9609df6
Divyam <71552681+divyam4070@users.noreply.github.com>
2025-05-29 00:39:15 +0530

    Time: 1 ms (76.27%), Space: 43.3 MB (99.26%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.java
+++ 0174-dungeon-game/0174-dungeon-game.java
@@ -1,43 +1,55 @@
 class Solution {
     public int calculateMinimumHP(int[][] dungeon) {
-        int m = dungeon.length;
-        int n = dungeon[0].length;
-        for(int i = m-1; i>=0; i--){
-            for(int j = n-1; j>=0; j--){
-                if(i==m-1 && j==n-1){  
-                    if(dungeon[i][j]>0){
-                        dungeon[i][j]=0;
-                    }
+        for(int i = dungeon.length-1; i>=0; i--){
+            for(int j = dungeon[0].length-1; j>=0; j--){
+                // if(i==dungeon.length-1 && j==dungeon[0].length-1){  
+                //     if(dungeon[i][j]>0){
+                //         dungeon[i][j]=0;
+                //     }
 
-                }
-                else if(i==m-1){
-                    if(dungeon[i][j]+dungeon[i][j+1]>0){
-                        dungeon[i][j]=0;
-                    }
-                    else{
-                        dungeon[i][j] = dungeon[i][j]+dungeon[i][j+1];
-                    }
-                }
-                else if(j == n-1){
-                    if(dungeon[i][j]+dungeon[i+1][j]>0){
-                        dungeon[i][j]=0;
-                    }
-                    else{
-                        dungeon[i][j] =dungeon[i][j]+dungeon[i+1][j];
-                    }
-                }
-                else{
-                    if(Math.max(dungeon[i][j]+dungeon[i+1][j], dungeon[i][j]+dungeon[i][j+1])>0){
-                        dungeon[i][j]=0;
-                    }
-                    else{
-                        dungeon[i][j] = Math.max(dungeon[i][j]+dungeon[i+1][j], dungeon[i][j]+dungeon[i][j+1]);
-                    }
-                }
+                // }
+                // else if(i==dungeon.length-1){
+                //     if(dungeon[i][j]+dungeon[i][j+1]>0){
+                //         dungeon[i][j]=0;
+                //     }
+                //     else{
+                //         dungeon[i][j] = dungeon[i][j]+dungeon[i][j+1];
+                //     }
+                // }
+                // else if(j == dungeon[0].length-1){
+                //     if(dungeon[i][j]+dungeon[i+1][j]>0){
+                //         dungeon[i][j]=0;
+                //     }
+                //     else{
+                //         dungeon[i][j] =dungeon[i][j]+dungeon[i+1][j];
+                //     }
+                // }
+                // else{
+                //     if(Math.max(dungeon[i][j]+dungeon[i+1][j], dungeon[i][j]+dungeon[i][j+1])>0){
+                //         dungeon[i][j]=0;
+                //     }
+                //     else{
+                //         dungeon[i][j] = Math.max(dungeon[i][j]+dungeon[i+1][j], dungeon[i][j]+dungeon[i][j+1]);
+                //     }
+                // }
+                dungeon[i][j] = helper(dungeon,i,j);
             }
         }
 
 
         return Math.abs(dungeon[0][0])+1;
+    }
+    int helper(int[][] dungeon, int i, int j){
+        if(i==dungeon.length-1 && j == dungeon[0].length-1)
+            return dungeon[i][j]>0 ? 0 : dungeon[i][j];
+        
+        if(i==dungeon.length-1)
+            return dungeon[i][j]+dungeon[i][j+1]>0 ? 0: dungeon[i][j]+dungeon[i][j+1];
+
+        if(j==dungeon[0].length-1)
+            return dungeon[i][j]+dungeon[i+1][j]>0 ? 0: dungeon[i][j]+dungeon[i+1][j];
+        
+        return Math.max(dungeon[i][j]+dungeon[i+1][j], dungeon[i][j]+dungeon[i][j+1])>0 ? 0 : Math.max(dungeon[i][j]+dungeon[i+1][j], dungeon[i][j]+dungeon[i][j+1]);
+        
     }
 }
