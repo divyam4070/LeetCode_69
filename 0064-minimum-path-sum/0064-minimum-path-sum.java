282e4fbf852275d6241676d242388789558e178f
Divyam <71552681+divyam4070@users.noreply.github.com>
2025-05-29 18:43:31 +0530

    Time: 3 ms (72.73%), Space: 47.7 MB (45.11%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -2,23 +2,35 @@ class Solution {
     public int minPathSum(int[][] grid) {
         for(int i=grid.length-1; i>=0;i--){
             for(int j = grid[0].length-1; j>=0;j--){
-
-                if(i==grid.length-1 && j==grid[0].length-1){
-                    continue;
-                }
-                else if(i == grid.length-1){
-                    grid[i][j] =grid[i][j]+grid[i][j+1] ;
-                }
-                else if(j==grid[0].length-1){
-                    grid[i][j] = grid[i][j]+grid[i+1][j];
-                }
-                else{
-                    grid[i][j] = grid[i][j]+ Math.min(grid[i+1][j], grid[i][j+1]);
-                }
+                grid[i][j] = helper(grid, i, j);
+                // if(i==grid.length-1 && j==grid[0].length-1){
+                //     continue;
+                // }
+                // else if(i == grid.length-1){
+                //     grid[i][j] =grid[i][j]+grid[i][j+1] ;
+                // }
+                // else if(j==grid[0].length-1){
+                //     grid[i][j] = grid[i][j]+grid[i+1][j];
+                // }
+                // else{
+                //     grid[i][j] = grid[i][j]+ Math.min(grid[i+1][j], grid[i][j+1]);
+                // }
             }
         }
       
 
         return grid[0][0];
+    }
+    int helper(int[][] grid, int i, int j){
+        if(i==grid.length-1 && j == grid[0].length-1){
+            return grid[i][j];
+        }
+        if(i==grid.length-1){
+            return grid[i][j]+grid[i][j+1];
+        }
+        if(j==grid[0].length-1){
+            return grid[i][j]+grid[i+1][j];
+        }
+        return grid[i+1][j]>grid[i][j+1] ? grid[i][j]+grid[i][j+1]:grid[i][j]+grid[i+1][j];
     }
 }
